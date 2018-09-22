763235be3218939ede0e3c43a426b168d1ab2084
evpodrezenko <evpodrezenko@gmail.com>
2018-09-22 16:07:44 +0300

    The task Sorter is implemented

--- src/index.js
+++ src/index.js
@@ -1,30 +1,47 @@
 class Sorter {
+
   constructor() {
-    // your implementation
+    this.array = new Array;
+    this.numComparator = function compareNum (left, right) {
+      return left - right;
+    };
+    // set default comparator
+    this.setComparator(this.numComparator);
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
-  sort(indices) {
-    // your implementation
+  sort(indices) {    
+    var tempArr = [];
+    indices.sort(this.numComparator);
+    
+    for (var i = 0; i < indices.length; i++) {
+      tempArr.push(this.at(indices[i]));
+    } 
+
+    tempArr.sort(this.comparator);
+    
+    for (var i = 0; i < indices.length; i++) {
+     this.array[indices[i]] = tempArr[i];
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
   }
 }
 
